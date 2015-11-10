ab2c63243392a2b04b107d200f82afca1a3e2237
richelbilderbeek <richel@richelbilderbeek.nl>
2015-11-10 17:07:25 +0100

    EOD

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weffc++"
-#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #include "graycodermenudialog.h"
-
-#pragma GCC diagnostic pop
 
 int main(int argc, char * argv[])
 {
-  const std::vector<std::string> args { ribi::grco::menu_dialog::ConvertArguments(argc,argv) };
+  const std::vector<std::string> args = { ribi::grco::menu_dialog::ConvertArguments(argc,argv) };
   ribi::grco::menu_dialog d;
   return d.Execute(args);
 }
